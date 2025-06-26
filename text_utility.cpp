952aa069563684f6c43f1d697e9339af7b051730
OmarMubarak7 <Mubarak@aucegypt.edu>
2025-06-26 19:04:22 +0300

    Added Word Count

--- text_utility.cpp
+++ text_utility.cpp
@@ -4,6 +4,39 @@
 #include "text_utility.h"
 using namespace std;
 
+
+#include <iostream>
+#include <cctype>
+#include <cstring>
+#include "text_utility.h"
+using namespace std;
+
+
+int countwords(char* str) {
+    
+    if (str[0] == '\0') return 0;
+
+    int count = 0;
+    int i = 0;
+
+    while (str[i] != '\0') {
+
+        while (str[i] == ' ') {
+            i++;
+        }
+
+        if (str[i] != '\0') {
+            count++;
+
+            while (str[i] != ' ' && str[i] != '\0') {
+                i++;
+            }
+        }
+    }
+
+    return count;
+}
+
 int characterCount(char* str) {
     int count = 0;
     while (str[count] != '\0') {
