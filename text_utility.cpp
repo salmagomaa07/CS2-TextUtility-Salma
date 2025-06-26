7ce2aca5af4bd204e4b3819577a4d0bcc8be1bcd
salmagomaa07 <salmagomaa@aucegypt.edu>
2025-06-26 17:56:38 +0300

    added character count function

--- text_utility.cpp
+++ text_utility.cpp
@@ -3,3 +3,11 @@
 #include <cstring>
 #include "text_utility.h"
 using namespace std;
+
+int characterCount(char* str) {
+    int count = 0;
+    while (str[count] != '\0') {
+        count++;
+    }
+    return count;
+}
