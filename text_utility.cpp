efbb5bd704c7239a45da597ba4f88ea21041f045
OmarMubarak7 <Mubarak@aucegypt.edu>
2025-06-26 19:39:58 +0300

    Reverse string function added

--- text_utility.cpp
+++ text_utility.cpp
@@ -4,6 +4,19 @@
 #include "text_utility.h"
 using namespace std;
 
+void reverseString(char* str) {
+    int start = 0;
+    int end = strlen(str) - 1; 
+
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        start++;
+        end--;
+    }
+}
 
 
 void VowelConsonantCount(char* str, int& vowels, int& consonants){
