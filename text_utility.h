9778e86caf54214a7ab2abcc4fc9eab39eec3215
salmagomaa07 <salmagomaa@aucegypt.edu>
2025-06-26 14:57:18 +0300

    Added text_utility.h

---
+++ text_utility.h
@@ -0,0 +1,16 @@
+int countwords(char*);
+
+int characterCount(char*);
+
+void VowelConsonantCount(char*, int&, int&);
+
+void reverseString(char*);
+
+bool isPalindrome(char*);
+
+void toUppercase(char*);
+
+void toLowercase(char*);
+
+int countSubstringOccurrences(char*, char*);
+
